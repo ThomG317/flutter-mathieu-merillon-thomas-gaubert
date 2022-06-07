957dbfa9a7d7a0a28c925c4317d7c1e9e96cb037
Thomas Gaubet <thomas.gaubert49@gmail.com>
2022-06-07 11:27:19 +0200

    feat: TP2

--- 02-ionic-core/src/index.js
+++ 02-ionic-core/src/index.js
@@ -9,6 +9,32 @@ const init = async () => {
     await defineCustomElements()
 
     // TODO
+    fetch("https://devfest-nantes-2018-api.cleverapps.io/blog")
+        .then(response => response.json())
+        .then(posts => {
+            const content = document.getElementById("content");
+            let inner = "";
+            posts.forEach(post => {
+                const card = `
+                <ion-card id="card" mode="ios">
+                  <img src="https://devfest2018.gdgnantes.com/${post.image}" alt="${post.image}"/>
+                  <ion-card-header>
+                    <ion-card-title>${post.title}</ion-card-title>
+                    <!--<div class="ion-text-center">
+                      <ion-button shape="round" mode="ios" color="primary">
+                        <ion-icon slot="icon-only" name="camera"></ion-icon>
+                      </ion-button>
+                    </div>-->
+                  </ion-card-header>
+            
+                  <ion-card-content>
+                   ${post.brief}
+                  </ion-card-content>
+                </ion-card>`;
+                inner += card;
+            });
+            content.innerHTML = inner;
+        });
 
 }
 
